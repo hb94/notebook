23bf6491a1480f4a341fb2a79c08ac8c37ddc187
SvenMaderer <Sven.Maderer@haw-hamburg.de>
2015-11-20 12:03:53 +0100

    Ziele programmiert...

--- MiniGame Versuch/minigame.js
+++ MiniGame Versuch/minigame.js
@@ -1,13 +1,13 @@
 var background = document.getElementById('background_can');
 var backgroundCTX = background.getContext('2d');
+var mainCan = document.getElementById('main_can');
+var mainCanCTX = mainCan.getContext('2d');
 
 var reX = 540;
 var reY = 710;
 var isPlaying = false;
+var ZielRechtecke = new Array();
 	
-
-
-
 var mouseX = 0;
 var mouseY = 0;
 
@@ -20,7 +20,8 @@ function mouse(e){
 
 function startGame(){
 	isPlaying = true;
-	play();
+	zieleErstellen();
+	playLoop();
 	
 }
 
@@ -28,21 +29,43 @@ function stoppGame(){
 	isPlaying=false;
 	
 }
-function play (){
+function playLoop (){
 	
 	if (isPlaying){
+				
 		reX=mouseX;
 		clearMainCan();
 		backgroundCTX.fillStyle = "red";
 		backgroundCTX.fillRect (reX , reY, 50,10);
-		window.setTimeout(play,10);
-		console.log(1);
+		window.setTimeout(playLoop,10);
+		
 		
 	
 		
 	}
 }
-
+function ZielRechteck(){
+	this.width= 20;
+	this.height= 20;
+	this.draw = function(xK,yK){
+		mainCanCTX.fillStyle = "black";
+		mainCanCTX.fillRect (xK,yK, this.width, this.height);
+	};
+}  
+function zieleErstellen(){
+	
+	for (var yCount = 0; yCount < 10;yCount++){
+		for (var i = 10; i <= 1050; i+=25){   /*breite der bisherigen Rechtecke plus Abstand; abgezogen von der maximalen Breite */
+			
+			var newRechteck = new ZielRechteck();
+			var xKS = 17+(i-10)/25*(newRechteck.width+5);
+			var yKS = 10+yCount* (newRechteck.height+5);
+			ZielRechtecke[i] = newRechteck;
+			ZielRechtecke[i].draw(xKS,yKS);
+			console.log(1);
+		};
+	}
+}
 function clearMainCan(){
 	backgroundCTX.clearRect(0,0,1080 ,720 );
 }
